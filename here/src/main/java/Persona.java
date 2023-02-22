f6f88a17b3a6ca8ff5f1972cc30def65010aedc8
DyaneJG <93893094+DyaneJG@users.noreply.github.com>
2023-02-22 00:06:20 -0600

    Add files via upload

---
+++ here/src/main/java/Persona.java
@@ -0,0 +1,59 @@
+
+public class Persona{
+
+    private int id;
+    private String nombre;
+    private String apellido_Peterno;
+    private String apellido_Materno;
+    private String Sexo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_Peterno() {
+        return apellido_Peterno;
+    }
+
+    public void setApellido_Peterno(String apellido_Peterno) {
+        this.apellido_Peterno = apellido_Peterno;
+    }
+
+    public String getApellido_Materno() {
+        return apellido_Materno;
+    }
+
+    public void setApellido_Materno(String apellido_Materno) {
+        this.apellido_Materno = apellido_Materno;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", nombre=" + nombre 
+                + ", apellido_Peterno=" + apellido_Peterno + 
+                ", apellido_Materno=" + apellido_Materno + ","
+                + " Sexo=" + Sexo + '}';
+    }
+
+   }
---
+++ here/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Alumno"},{"p":"<Unnamed>","l":"Libros"},{"p":"<Unnamed>","l":"Persona"},{"p":"<Unnamed>","l":"Prestamo"},{"p":"<Unnamed>","l":"Profesor"}];updateSearchResults();
