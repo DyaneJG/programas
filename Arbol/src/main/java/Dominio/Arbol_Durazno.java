c3e24fc0884b7fda95fcde6042e419c235f80c68
DyaneJG <93893094+DyaneJG@users.noreply.github.com>
2023-03-28 23:23:28 -0600

    Add files via upload

---
+++ Arbol/src/main/java/Dominio/Arbol_Durazno.java
@@ -0,0 +1,9 @@
+//Tania Dyane Jimenez Granillo
+package Dominio;
+public class Arbol_Durazno extends Arbol {
+
+    public Arbol_Durazno(Integer numero, Integer numeroFrutas, Integer alturaCm, Integer pesoPromedioFruta) {
+        super(numero, numeroFrutas, alturaCm, pesoPromedioFruta);
+    }
+    
+}
---
+++ Arbol/src/main/java/Test/Manzana.java
@@ -0,0 +1,12 @@
+//Tania Dyane Jimenez Granillo
+package Test;
+
+import Vista.Vista_Manzana;
+public class Manzana {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vista_Manzana vistaManzana = new Vista_Manzana();
+        vistaManzana.run();
+    }
+    
+}
